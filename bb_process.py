5cda337bad5a2bf9a73adf8476b2cd7522cb85ea
Chu Ngoc Vuong <21020674@vnu.edu.vn>
2024-07-05 16:41:47 +0700

    Draw by frame

--- bb_process.py
+++ bb_process.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
 box_file = open("video1_15fps_gt.txt")
-for i in range(1, 50, 1):
+# for i in range(1, 50, 1):
 
-    box = box_file.readlin
-    box_arr = box.split(',')
+#     box = box_file.readlin
+#     box_arr = box.split(',')
 
-    min_cor = (box_arr[2], box_arr[3])
-    max_cor = (box_arr[4], box_arr[5])
-    # print(min_cor)
+#     min_cor = (box_arr[2], box_arr[3])
+#     max_cor = (box_arr[4], box_arr[5])
+#     # print(min_cor)
 
 box_df = pd.read_csv(box_file, header = None)
 
 print(box_df.head())
-
+frame_data = box_df[box_df[0] == 151]
+for i, row in frame_data.iterrows():
+    print(row[2])
 box_file.close()
 
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import cv2
+import pandas as pd
 
 cap = cv2.VideoCapture("demovideo1_test.mp4")
-box_file = open("video1_15fps_gt.txt")
+box_file = open("video1_15fps_t.txt")
+box_df = pd.read_csv(box_file, header = None)
 
 while(True):
     ret, frame = cap.read()
 
     pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+    frame_data = box_df[box_df[0] == pos_frame]
+    print(pos_frame)
+    for i, row in frame_data.iterrows():
+        min_cor = (round(row[2]), round(row[3]))
+        max_cor = (round(row[4] + row[2]), round(row[5] + row[3]))
 
-    box = box_file.readline()
-    box_arr = box.split(',')
-    box_arr = [float(i) for i in box_arr]
-    min_cor = (round(box_arr[2]), round(box_arr[3]))
-    max_cor = (round(box_arr[4] + box_arr[2]), round(box_arr[5] + box_arr[3]))
-
-    if(pos_frame ==  box_arr[0]):
         frame = cv2.rectangle(frame, min_cor, max_cor, (255,0,0), 1)
 
-    print(pos_frame, box_arr[0])  
     if (ret == True):
         cv2.imshow("vid", frame)
 
